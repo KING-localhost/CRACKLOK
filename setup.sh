534bd00158c37b98cfd254aeaf8a6665e34d5c17
KING-localhost <dd4460484@gmail.com>
2020-10-23 22:27:25 +0700

    upload dari termux

---
+++ setup.sh
@@ -0,0 +1,16 @@
+pkg install figlet
+pkg install toilet
+figlet PLEASE WAIT...
+sleep 5
+clear
+pkg update && pkg upgrade
+pkg install php
+pkg install openssh
+pkg install dropbear
+sleep 5
+clear
+figlet SUCCESFULL 
+toilet INSTALING
+figlet WAIT...
+sleep 10
+clear
